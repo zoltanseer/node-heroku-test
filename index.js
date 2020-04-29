b58bfa2c97fc89243cc9242ddf3469d013896ee0
zoltanseer <zoltan.seer@gmail.com>
2020-04-29 11:41:43 +0300

    express test

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/", function (req, res) {
+  res.send("Got a POST request");
+});
+
+app.put("/user", function (req, res) {
+  res.send("Got a PUT request at /user");
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
